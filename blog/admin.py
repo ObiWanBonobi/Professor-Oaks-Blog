46bd6b480bee3b524d39c2a9aaa0dc8fe7d897e2
Bo de Groot <bclm.degroot@gmail.com>
2024-02-15 14:14:28 +0100

    Add admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+""" Admin for the blog page """
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+admin.site.register(Post)
